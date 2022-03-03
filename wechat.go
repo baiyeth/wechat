1130490375c1d570f8ea344d4bb77bfd0603da47
skybosi <skybosi_ok@163.com>
2022-03-03 23:36:30 +0800

    open Wechat struct

--- wechat.go
+++ wechat.go
@@ -11,22 +11,22 @@ import (
 	"github.com/baiyeth/wechat/pay"
 )
 
-type wechat struct {
+type Wechat struct {
 	ctx context.Context
 	wc  *wechat2.Wechat
 	cfg conf.Configuration
 }
 
-type Option func(*wechat)
+type Option func(*Wechat)
 
 func WithConfiguration(cfg conf.Configuration) Option {
-	return func(m *wechat) {
+	return func(m *Wechat) {
 		m.cfg = cfg
 	}
 }
 
 func WithOfficialTplMessage(Expr, Users, TemplateId, MiniProgramAppId, MiniProgramPagePath string) Option {
-	return func(m *wechat) {
+	return func(m *Wechat) {
 		m.cfg.Official.TplMessage.Expr = Expr
 		m.cfg.Official.TplMessage.Users = Users
 		m.cfg.Official.TplMessage.TemplateId = TemplateId
@@ -36,7 +36,7 @@ func WithOfficialTplMessage(Expr, Users, TemplateId, MiniProgramAppId, MiniProgr
 }
 
 func WithAuth(AppId, AppSecret string) Option {
-	return func(m *wechat) {
+	return func(m *Wechat) {
 		m.cfg.Official = conf.OfficialConfiguration{
 			AppId:     AppId,
 			AppSecret: AppSecret,
@@ -49,7 +49,7 @@ func WithAuth(AppId, AppSecret string) Option {
 }
 
 func WithPayConfig(appId, appSecret, mchId, key, notifyUrl string) Option {
-	return func(m *wechat) {
+	return func(m *Wechat) {
 		m.cfg.Pay = conf.PayConfiguration{
 			AppId:     appId,
 			AppSecret: appSecret,
@@ -57,34 +57,42 @@ func WithPayConfig(appId, appSecret, mchId, key, notifyUrl string) Option {
 			Key:       key,
 			NotifyURL: notifyUrl,
 		}
+		m.cfg.Official = conf.OfficialConfiguration{
+			AppId:     appId,
+			AppSecret: appSecret,
+		}
+		m.cfg.MiniProgram = conf.MiniProgramConfiguration{
+			AppId:     appId,
+			AppSecret: appSecret,
+		}
 	}
 }
 
-func NewWechat(ctx context.Context, opts ...Option) wechat {
-	w := wechat{
+func NewWechat(ctx context.Context, opts ...Option) *Wechat {
+	w := &Wechat{
 		ctx: ctx,
 		wc:  wechat2.NewWechat(),
 		cfg: conf.Configuration{},
 	}
 	for _, o := range opts {
-		o(&w)
+		o(w)
 	}
 	return w
 }
 
-func NewDefaultWechat(ctx context.Context, AppId, AppSecret string) wechat {
+func NewDefaultWechat(ctx context.Context, AppId, AppSecret string) *Wechat {
 	return NewWechat(ctx, WithAuth(AppId, AppSecret))
 }
 
-func (w *wechat) GetMiniProgram() miniProgram.MiniProgram {
+func (w *Wechat) GetMiniProgram() miniProgram.MiniProgram {
 	return w.getMiniProgram(w.cfg.MiniProgram.AppId, w.cfg.MiniProgram.AppSecret)
 }
 
-func (w *wechat) GetOfficialAccount() officialAccount.OfficialAccount {
+func (w *Wechat) GetOfficialAccount() officialAccount.OfficialAccount {
 	return w.getOfficialAccount(w.cfg.Official.AppId, w.cfg.Official.AppSecret)
 }
 
-func (w *wechat) GetPay() pay.Pay {
+func (w *Wechat) GetPay() pay.Pay {
 	return w.getPay(pay.Config{
 		AppID:     w.cfg.Pay.AppId,
 		MchID:     w.cfg.Pay.MchID,
@@ -93,18 +101,18 @@ func (w *wechat) GetPay() pay.Pay {
 	})
 }
 
-func (w *wechat) GetCfg() conf.Configuration {
+func (w *Wechat) GetCfg() conf.Configuration {
 	return w.cfg
 }
 
-func (w *wechat) getMiniProgram(AppId, AppSecret string) miniProgram.MiniProgram {
+func (w *Wechat) getMiniProgram(AppId, AppSecret string) miniProgram.MiniProgram {
 	return miniProgram.NewDefaultMiniProgram(w.ctx, w.wc, AppId, AppSecret)
 }
 
-func (w *wechat) getOfficialAccount(AppId, AppSecret string) officialAccount.OfficialAccount {
+func (w *Wechat) getOfficialAccount(AppId, AppSecret string) officialAccount.OfficialAccount {
 	return officialAccount.NewOfficialAccount(w.ctx, w.wc, officialAccount.WithOfficialAccountAuth(AppId, AppSecret))
 }
 
-func (w *wechat) getPay(cfg pay.Config) pay.Pay {
+func (w *Wechat) getPay(cfg pay.Config) pay.Pay {
 	return pay.NewPay(w.ctx, w.wc, pay.WithPayConfig(cfg.AppID, cfg.MchID, cfg.Key, cfg.NotifyURL))
 }
